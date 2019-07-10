064d6ff27e58b45c1dc7e5e8881124f023df75c5
nelson.aleluya <aleluyaerwin@gmail.com>
2019-07-10 17:24:43 -0400

    Adaptacion del template Admin LTE

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,76 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Alexander Pierce</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAPA DEL SITIO</li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-list-ul"></i> <span>Inicio</span>
+          </a>
+        </li>
+        <li class="treeview">
+            <a href="#">
+              <i class="fa fa-th"></i> <span>Usuarios</span>
+              <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+            <ul class="treeview-menu">
+                <li><a href="../layout/top-nav.html"><i class="fa fa-circle-o"></i> Adminitradores</a></li>
+                <li><a href="../layout/boxed.html"><i class="fa fa-circle-o"></i> Superiores</a></li>
+                <li><a href="../layout/fixed.html"><i class="fa fa-circle-o"></i> Funcionarios</a></li>
+              </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-bank"></i>
+            <span>Ddependencias</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="../layout/top-nav.html"><i class="fa fa-circle-o"></i> Secretarias</a></li>
+            <li><a href="../layout/boxed.html"><i class="fa fa-circle-o"></i> Direcciones</a></li>
+            <li><a href="../layout/fixed.html"><i class="fa fa-circle-o"></i> Unidades</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-pie-chart"></i>
+            <span>Reportes</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="../charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
+            <li><a href="../charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
+          </ul>
+        </li>        
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
---
+++ resources/views/usuarios/administradores.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.template')
+
+@section('css')
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') }}">
+@endsection
+@section('content')
+<section class="content-header">
+        <h1>
+          Principal
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Registros</a></li>
+        </ol>
+      </section>
+  
+      <!-- Main content -->
+    <section class="content">
+            <div class="row">
+              <div class="col-xs-12">      
+                <div class="box">
+                  <div class="box-header">
+                    <h3 class="box-title">Todos los archivos</h3>
+                  </div>
+                  <!-- /.box-header -->
+                  <div class="box-body">
+                    <table id="example1" class="table table-bordered table-hover">
+                      <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Funcionario</th>
+                        <th>Cargo</th>
+                        <th>Tipo Registro</th>
+                        <th>Horas<br>marcación</th>
+                        <th>Acción</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 4.0
+                        </td>
+                        <td>Win 95+</td>
+                        <td> 4</td>
+                        <td>X</td>
+                        <td>X</td>
+                      </tr>
+                      </tbody>
+                      <tfoot>
+                      <tr>
+                        <th>#</th>
+                        <th>Funcionario</th>
+                        <th>Cargo</th>
+                        <th>Tipo Registro</th>
+                        <th>Horas<br>marcación</th>
+                        <th>Accion</th>
+                      </tr>
+                      </tfoot>
+                    </table>
+                  </div>
+                  <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+              </div>
+              <!-- /.col -->
+            </div>
+            <!-- /.row -->
+          </section>
+          <!-- /.content -->
+      @section('js')
+            <!-- DataTables -->
+            <script src="{{ asset('bower_components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+            <script src="{{ asset('bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
+            <script>
+                    $(function () {
+                      $('#example1').DataTable()
+                    })
+                  </script>
+      @endsection
+@endsection
